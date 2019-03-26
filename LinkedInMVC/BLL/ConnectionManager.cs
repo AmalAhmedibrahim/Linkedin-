bc6952eefc7d19cfd9870fc7d9d57a49ce29b48d
Shimaa Adel <shimaa.a44@yahoo.com>
2019-03-26 06:02:09 -0700

    add view model folder and view connection

--- LinkedInMVC/BLL/ConnectionManager.cs
+++ LinkedInMVC/BLL/ConnectionManager.cs
@@ -9,8 +9,20 @@ namespace LinkedInMVC.BLL
 {
     public class ConnectionManager : Repository<Company, ApplicationDbContext>
     {
+        private ApplicationDbContext context;
         public ConnectionManager(ApplicationDbContext context) : base(context)
         {
+            context = this.context;
+
+        }
+        public void AddFriendRequest( ApplicationUser userId, ApplicationUser connectionId)
+        {
+            Connection_Request friend = new Connection_Request();
+            friend.FK_UserId = userId;
+            friend.FK_Connction_UserId = connectionId;
+            friend.IsApproved = false;
+            context.Connection_Requeset.Add(friend);
+            context.SaveChanges();
 
         }
     }
--- LinkedInMVC/Controllers/ConnectionController.cs
+++ LinkedInMVC/Controllers/ConnectionController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LinkedInMVC.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,18 @@ namespace LinkedInMVC.Controllers
     public class ConnectionController : Controller
     {
         // GET: Connection
+        public UnitofWork UnitofWork
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<UnitofWork>();
+            }
+        }
         public ActionResult Index()
         {
+            var u = UnitofWork.UserManager.Users.ToList();
             return View();
         }
+
     }
 }
